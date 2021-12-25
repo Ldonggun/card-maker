25d97a653a918d251a937c48263e679dae5eef30
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2021-12-26 03:10:04 +0900

    Add. Modal components

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './modal.module.css';
+const Modal = props => {
+  return (
+    <>
+      <div>Modal</div>
+    </>
+  );
+};
+
+export default Modal;
