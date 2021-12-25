e97bb67e569099056ad7fb328a2e38145f9cb917
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2021-12-26 03:20:20 +0900

    Add. header components

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-import "./app.css";
-
+import React from 'react';
+import './app.css';
+//components
+import { Modal } from './components/index';
 function App() {
-  return <h1>Hello :)</h1>;
+  return (
+    <>
+      <Modal></Modal>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import style from './header.module.css'
+const Header = (props) => (
+      
+  );
+
+export default Header;
